fad6d925856195e5c6bfc31a66a30db768568974
usbangelo <50382278+usbangelo@users.noreply.github.com>
2019-11-03 18:34:59 -0300

    Campeonatos, Pagina de Campeonato e Formulario de campeonato

--- www/js/controllers/appCtrl.js
+++ www/js/controllers/appCtrl.js
@@ -19,23 +19,6 @@ angular.module('app')
       $scope.ajudaModal.hide();
     };
 
-    // cria e carrega o modal de e-sports
-    $ionicModal.fromTemplateUrl('templates/e-sports.html', function (modal) {
-      $scope.esportsModal = modal;
-    }, {
-      scope: $scope,
-    });
-
-    // abre o modal de e-sports
-    $scope.abreEsports = function () {
-      $scope.esportsModal.show();
-    };
-
-    // fecha o novo modal de e-sports
-    $scope.fechaEsports = function () {
-      $scope.esportsModal.hide();
-    };
-
     // cria e carrega o modal de esportes
     $ionicModal.fromTemplateUrl('templates/esportes.html', function (modal) {
       $scope.esportesModal = modal;
@@ -51,6 +34,23 @@ angular.module('app')
     // fecha o novo modal de esportes
     $scope.fechaEsportes = function () {
       $scope.esportesModal.hide();
+    };
+
+    // cria e carrega o modal de e-sports
+    $ionicModal.fromTemplateUrl('templates/e-sports.html', function (modal) {
+      $scope.esportsModal = modal;
+    }, {
+      scope: $scope,
+    });
+
+    // abre o modal de e-sports
+    $scope.abreEsports = function () {
+      $scope.esportsModal.show();
+    };
+
+    // fecha o novo modal de e-sports
+    $scope.fechaEsports = function () {
+      $scope.esportsModal.hide();
     };
 
     // cria e carrega o modal de e-sports1
@@ -87,38 +87,89 @@ angular.module('app')
       $scope.esportesModal1.hide();
     };
 
-    // cria e carrega o modal de equipes
-    $ionicModal.fromTemplateUrl('templates/equipes.html', function (modal) {
-      $scope.equipesModal = modal;
+    // cria e carrega o modal de campeonatos
+    $ionicModal.fromTemplateUrl('templates/campeonatos.html', function (modal) {
+      $scope.campeonatosModal = modal;
     }, {
       scope: $scope,
     });
 
-    // abre o modal de equipes
-    $scope.abreEquipes = function () {
-      $scope.equipesModal.show();
+    // abre o modal de campeonatos
+    $scope.abreCampeonatos = function () {
+      $scope.campeonatosModal.show();
     };
 
-    // fecha o novo modal de equipes
-    $scope.fechaEquipes = function () {
-      $scope.equipesModal.hide();
+    // fecha o novo modal de campeonatos
+    $scope.fechaCampeonatos = function () {
+      $scope.campeonatosModal.hide();
     };
 
-    // cria e carrega o modal de formulario equipe
-    $ionicModal.fromTemplateUrl('templates/formulario_equipe.html', function (modal) {
-      $scope.formulario_equipeModal = modal;
+    // cria e carrega o modal de campeonato
+    $ionicModal.fromTemplateUrl('templates/campeonato.html', function (modal) {
+      $scope.campeonatoModal = modal;
     }, {
       scope: $scope,
     });
 
-    // abre o modal de formulario equipe
-    $scope.abreFomularioEquipe = function () {
-      $scope.formulario_equipeModal.show();
+    // abre o modal de campeonato
+    $scope.abreCampeonato = function () {
+      $scope.campeonatoModal.show();
     };
 
-    // fecha o novo modal de formulario equipe
-    $scope.fechaFormularioEquipe = function () {
-      $scope.formulario_equipeModal.hide();
+    // fecha o novo modal de campeonato
+    $scope.fechaCampeonato = function () {
+      $scope.campeonatoModal.hide();
     };
-    
+
+    // cria e carrega o modal de tabela
+    $ionicModal.fromTemplateUrl('templates/tabela.html', function (modal) {
+      $scope.tabelaModal = modal;
+    }, {
+      scope: $scope,
+    });
+
+    // abre o modal de tabela
+    $scope.abreTabela = function () {
+      $scope.tabelaModal.show();
+    };
+
+    // fecha o novo modal de tabela
+    $scope.fechaTabela = function () {
+      $scope.tabelaModal.hide();
+    };
+
+    // cria e carrega o modal de eliminatoria
+    $ionicModal.fromTemplateUrl('templates/eliminatoria.html', function (modal) {
+      $scope.eliminatoriaModal = modal;
+    }, {
+      scope: $scope,
+    });
+
+    // abre o modal de eliminatoria
+    $scope.abreEliminatoria = function () {
+      $scope.eliminatoriaModal.show();
+    };
+
+    // fecha o novo modal de eliminatoria
+    $scope.fechaEliminatoria = function () {
+      $scope.eliminatoriaModal.hide();
+    };
+
+    // cria e carrega o modal de formulario campeonato
+    $ionicModal.fromTemplateUrl('templates/formCampeonato.html', function (modal) {
+      $scope.formCampeonatoModal = modal;
+    }, {
+      scope: $scope,
+    });
+
+    // abre o modal de formulario campeonato
+    $scope.abreFormCampeonato = function () {
+      $scope.formCampeonatoModal.show();
+    };
+
+    // fecha o novo modal de formulario campeonato
+    $scope.fechaFormCampeonato = function () {
+      $scope.formCampeonatoModal.hide();
+    };
+
   })
