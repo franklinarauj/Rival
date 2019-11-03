2e156934ee0681a40eb7f2d22597e45bc16c142d
Franklin Araujo <franklinarauj@sempreceub.com>
2019-11-03 14:50:35 -0300

    Telas de Esporte e E-Sport

--- www/js/controllers/appCtrl.js
+++ www/js/controllers/appCtrl.js
@@ -34,14 +34,48 @@ angular.module('app')
       scope: $scope,
     });
 
-    // abre o modal de opções
+    // abre o modal de ajuda
     $scope.abreAjuda = function () {
       $scope.ajudaModal.show();
     };
 
-    // fecha o novo modal de opções
+    // fecha o novo modal de ajuda
     $scope.fechaAjuda = function () {
       $scope.ajudaModal.hide();
+    };
+
+    // cria e carrega o modal de esportes
+    $ionicModal.fromTemplateUrl('templates/esportes.html', function (modal) {
+      $scope.esportesModal = modal;
+    }, {
+      scope: $scope,
+    });
+
+    // abre o modal de esportes
+    $scope.abreEsportes = function () {
+      $scope.esportesModal.show();
+    };
+
+    // fecha o novo modal de esportes
+    $scope.fechaEsportes = function () {
+      $scope.esportesModal.hide();
+    };
+
+    // cria e carrega o modal de e-sports
+    $ionicModal.fromTemplateUrl('templates/e-sports.html', function (modal) {
+      $scope.esportsModal = modal;
+    }, {
+      scope: $scope,
+    });
+
+    // abre o modal de e-sports
+    $scope.abreEsports = function () {
+      $scope.esportsModal.show();
+    };
+
+    // fecha o novo modal de e-sports
+    $scope.fechaEsports = function () {
+      $scope.esportsModal.hide();
     };
 
     // cria e carrega o modal de nova opção
